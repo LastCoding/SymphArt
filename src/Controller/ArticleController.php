10fda589ede1022e77b6d0c3d1902f7a7396e8f8
LastPlays <inyakimd@gmail.com>
2018-05-08 17:05:49 +0200

    Article Structure coded & some save feature commented for a future ref

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -13,10 +14,27 @@ class ArticleController extends Controller {
 	 * @Method({"GET"})
 	 */
 	public function index() {
-//		return new Response( '' );
 
-		$articles = ['Article 1', 'Article 2'];
+		$articles = [ 'Article 1', 'Article 2' ];
 
-		return $this->render('articles/index.html.twig', array('articles' => $articles));
+		return $this->render( 'articles/index.html.twig', array( 'articles' => $articles ) );
 	}
+
+//	/**
+//	 * @Route("/article/save")
+//	 */
+//
+//	public function save() {
+//		$entityManager = $this->getDoctrine()->getManager();
+//
+//		$article = new Article();
+//		$article->setTitle('Article One');
+//		$article->setBody('This is the body for article one');
+//
+//		$entityManager->persist($article);
+//
+//		$entityManager->flush();
+//
+//		return new Response('Saves an article with the id of '. $article->getId());
+//	}
 }
